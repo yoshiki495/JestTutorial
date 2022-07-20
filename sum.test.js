9121f169499220bfb4d1526c3b6d015ddd866d5c
yoshiki495 <yoshiki-tanaka@cuebic.co.jp>
2022-07-20 15:56:44 +0900

    add test

--- sum.test.js
+++ sum.test.js
@@ -1,8 +1,18 @@
-// const sum = require('./sum');
+const sum = require('./sum');
 
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(sum(1, 2)).toBe(3);
-// });
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+});
+  
+test('object assignment', () => {
+    const data = {one: 1};
+    data['two'] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+});
 
 test('adding positive numbers is not zero', () => {
     for (let a = 1; a < 10; a++) {
@@ -10,8 +20,26 @@ test('adding positive numbers is not zero', () => {
         expect(a + b).not.toBe(0);
       }
     }
-  });
+});
 
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+});
+  
 test('two plus two', () => {
     const value = 2 + 2;
     expect(value).toBeGreaterThan(3);
@@ -22,6 +50,12 @@ test('two plus two', () => {
     // toBe and toEqual are equivalent for numbers
     expect(value).toBe(4);
     expect(value).toEqual(4);
+});
+
+test('adding floating point numbers', () => {
+    const value = 0.1 + 0.2;
+    //expect(value).toBe(0.3);           This won't work because of rounding error
+    expect(value).toBeCloseTo(0.3); // This works.
 });
   
 
